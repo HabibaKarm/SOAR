c827cd3e95f14b980b7c3cde1462617fe8c79064
HabibaKarm <habibakarm714@gmail.com>
2025-09-07 22:52:35 +0300

    BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL} >
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/" element={<Layout />}>
